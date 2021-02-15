e5a70a4e5cb0d6579dd4d725ab64fc410c4efb4a
Simon Beckx <simon.beckx@ixor.be>
2021-02-15 15:01:20 +0100

    Added Jira ticket creation & abstract to notification class to be more flexible

--- ASS/Notification.py
+++ ASS/Notification.py
@@ -1,22 +1,27 @@
 import os
 from httplib2 import Http
+from jira import JIRA
 from json import dumps
 
 
 class Notification:
 
     # Default value is the 1ste Lijn Support Google Chat channel
-    default_chat_url = "https://chat.googleapis.com/v1/spaces/AAAA7-V1xdw/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=Aju_BkmuyN6EMf7Y1x8yTpzvzT2QvJlckCP4Bpz8LIA%3D"
-    chat_url = os.getenv("CHATURL", default_chat_url)
+    chat_url = os.getenv('CHATURL')
 
-    if chat_url == "":
-        chat_url = default_chat_url
+    jira_url = os.getenv('JIRA_URL')
+    jira_user = os.getenv('JIRA_USER')
+    jira_password = os.getenv('JIRA_API_PASSWORD')
+
+    notificationmode = os.getenv('NOTIFICATION_MODE')
 
     @staticmethod
-    def post_message_to_google_chat(text: str):
+    def post_message_to_google_chat(summary: str, description: str):
         message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
         http_obj = Http()
+
+        text = f"{summary} \n{description}"
 
         return http_obj.request(
             uri=Notification.chat_url,
@@ -24,3 +29,26 @@ class Notification:
             headers=message_headers,
             body=dumps({'text': text})
         )
+
+    @staticmethod
+    def create_jira_ticket(summary: str, description: str):
+
+        options = {'server': Notification.jira_url}
+        jira = JIRA(options, basic_auth=(Notification.jira_user,Notification.jira_password))
+
+        issue_dict = {
+            'project': {'id': 16937},
+            'summary': f'{summary}',
+            'description': f'{description}',
+            'issuetype': {'name': 'Support'},
+        }
+
+        jira.create_issue(fields=issue_dict)
+
+    @staticmethod
+    def send_notification(summary: str, description: str = ""):
+
+        if Notification.notificationmode == 'GOOGLECHAT':
+            Notification.post_message_to_google_chat(summary, description)
+        elif Notification.notificationmode == 'JIRA':
+            Notification.create_jira_ticket(summary, description)
